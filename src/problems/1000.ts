743e12c74d9c629b9d1b6b2caceb6b708cbd00d7
igiza1213 <igiza1213@gmail.com>
2024-06-21 10:33:52 +0900

    init

---
+++ src/problems/1000.ts
@@ -0,0 +1,11 @@
+export const solution = (stdinInput: string) => {
+    const [N, M, circle] = stdinInput
+      .trim()
+      .split(" ")
+      .map((value) => parseInt(value));
+  
+      console.log(N, M, circle)
+  };
+  
+  // solution(input);
+  
