b85424795b934d24f5a39f63891eabc1dd72cf65
igiza1213 <igiza1213@gmail.com>
2024-06-27 00:30:21 +0900

    1002
    터렛 뿅

--- src/problems/1002.ts
+++ src/problems/1002.ts
@@ -5,23 +5,29 @@ var input = fs.readFileSync(__dirname + filePath).toString();
 export const solution = (stdinInput: string) => {
   const [range, ...testCases] = stdinInput
     .trim()
-    .split(" ")
-    .map((value) => parseInt(value));
+    .split("\n")
+    .map((line) => line.split(" ").map(Number));
 
-  let line = {
-    x1: testCases[0],
-    y1: testCases[0],
-    r1: testCases[0],
-    x2: testCases[0],
-    y2: testCases[0],
-    r2: testCases[0],
-  };
+  for (let i = 0; i < range[0]; i++) {
+    let [x1, y1, r1, x2, y2, r2] = testCases[i];
 
-  for (let i = 0; i < range; i++) {
-    testCases;
+    const dx = x2 - x1;
+    const dy = y2 - y1;
+    const distanceSquared = dx * dx + dy * dy;
+    const distance = Math.sqrt(distanceSquared);
+    const Sradius = r1 + r2;
+    const Dradius = Math.abs(r1 - r2);
+
+    if (distance === 0 && r1 === r2) {
+      console.log(-1); // 두 원이 동일한 경우
+    } else if (distance === Sradius || distance === Dradius) {
+      console.log(1); // 두 원이 외접하거나 내접하는 경우
+    } else if (distance < Dradius || distance > Sradius) {
+      console.log(0); // 두 원이 서로 떨어져 있는 경우
+    } else {
+      console.log(2); // 두 원이 두 점에서 만나는 경우
+    }
   }
-
-  console.log(range);
 };
 
 solution(input);
