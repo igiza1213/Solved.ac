7611edd09037838306e8a8da79122f2d0143e3cd
igiza1213 <igiza1213@gmail.com>
2024-07-11 14:45:01 +0000

    불찰

---
+++ src/problems/17249.ts
@@ -0,0 +1,16 @@
+import fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "/input.txt";
+var input = fs.readFileSync(__dirname + filePath).toString();
+
+export const solution = (stdinInput: string) => {
+  const s = stdinInput.trim();
+
+  console.log(
+    s
+      .split("0")
+      .map((value) => value.split("@").length - 1)
+      .join(" ")
+  );
+};
+
+solution(input);
--- src/problems/27434.ts
+++ src/problems/27434.ts
@@ -1,18 +1,92 @@
-// import fs = require("fs");
-// const filePath = process.platform === "linux" ? "/dev/stdin" : "/input.txt";
-// var input = fs.readFileSync(__dirname + filePath).toString();
+const rl = require("readline").createInterface(process.stdin, process.stdout);
+rl.on("line", (line: string) => {
+  console.log(factorial(+line).toString());
+  rl.close();
+});
 
-// export const solution = (stdinInput: string) => {
-//   const n = Number(stdinInput.trim());
+function factorial(number: number) {
+  const heap = new PriorityQueue();
+  for (let i = 0; i <= number; i++) heap.enqueue(BigInt(i == 0 ? 1 : i));
+  while (heap.heap.length > 1) {
+    heap.enqueue(heap.dequeue()!.data * heap.dequeue()!.data);
+  }
+  return heap.dequeue()!.data;
+}
 
-//   let num = BigInt(1);
-//   for (let i = 0; i < n; i++) {
-//     num *= BigInt(n - i);
-//   }
+class Heap {
+  /** @type Array<{ data: bigint; }> */
+  heap: Array<{ data: bigint }> = [];
 
-//   console.log(num.toString());
-// };
+  getLeftChildIndex = (parentIndex: number) => parentIndex * 2 + 1;
+  getRightChildIndex = (parentIndex: number) => parentIndex * 2 + 2;
+  getParentIndex = (childIndex: number) => Math.floor((childIndex - 1) / 2);
 
-// solution(input);
+  peek = () => this.heap[0];
 
-// 정신나간듯 이게 브론즈 5?
+  insert(data: bigint) {
+    const node = { data };
+    this.heap.push(node);
+    this.heapifyUp();
+  }
+
+  remove() {
+    const count = this.heap.length;
+    const rootNode = this.peek();
+
+    if (count <= 0) return undefined;
+    if (count === 1) this.heap = [];
+    else {
+      this.heap[0] = this.heap.pop()!;
+      this.heapifyDown();
+    }
+
+    return rootNode;
+  }
+
+  heapifyUp() {
+    let currentIndex = this.heap.length - 1;
+    const lastInsertedNode = this.heap[currentIndex];
+
+    while (currentIndex > 0) {
+      const parentIndex = this.getParentIndex(currentIndex);
+
+      if (this.heap[parentIndex].data > lastInsertedNode.data) {
+        this.heap[currentIndex] = this.heap[parentIndex];
+        currentIndex = parentIndex;
+      } else break;
+    }
+
+    this.heap[currentIndex] = lastInsertedNode;
+  }
+
+  heapifyDown() {
+    let index = 0;
+    const count = this.heap.length;
+    const rootNode = this.peek();
+
+    while (this.getLeftChildIndex(index) < count) {
+      const leftChildIndex = this.getLeftChildIndex(index);
+      const rightChildIndex = this.getRightChildIndex(index);
+      const smallerChildIndex =
+        rightChildIndex < count &&
+        this.heap[rightChildIndex].data < this.heap[leftChildIndex].data
+          ? rightChildIndex
+          : leftChildIndex;
+
+      if (this.heap[smallerChildIndex].data <= rootNode.data) {
+        this.heap[index] = this.heap[smallerChildIndex];
+        index = smallerChildIndex;
+      } else break;
+    }
+
+    this.heap[index] = rootNode;
+  }
+}
+
+class PriorityQueue extends Heap {
+  enqueue = (value: bigint) => this.insert(value);
+  dequeue = () => this.remove();
+  isEmpty = () => this.heap.length <= 0;
+}
+
+// 나에게는 아직 해로운 문제요 이상 여백은 마친다.
