64696b8916a5e21cfaebac129e0deb0774838401
msg2324 <73478662+igiza1213@users.noreply.github.com>
2024-08-08 22:03:39 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 104 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.ts
@@ -0,0 +1,10 @@
+import fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "/input.txt";
+var input = fs.readFileSync(__dirname + filePath).toString();
+
+export const solution = (stdinInput: string) => {
+  const S = stdinInput;
+  console.log(S);
+};
+
+solution(input);
