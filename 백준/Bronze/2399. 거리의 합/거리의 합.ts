bb8db8a93d2a800c063413e96e65ecd220ad4f80
msg2324 <73478662+igiza1213@users.noreply.github.com>
2024-08-08 21:49:05 +0900

    [Bronze II] Title: 거리의 합, Time: 204 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/2399. 거리의 합/거리의 합.ts
@@ -0,0 +1,31 @@
+import fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "/input.txt";
+var input = fs.readFileSync(__dirname + filePath).toString();
+
+const abs = (n: bigint): bigint => (n < 0 ? -n : n);
+
+export const solution = (stdinInput: string) => {
+  const [n, ...arr] = stdinInput.trim().split(/\s/).map(BigInt);
+
+  arr.sort((a, b) => Number(a - b));
+
+  let result: bigint = BigInt(0);
+
+  for (let i = 1; i < n; i++) {
+    result += (arr[i] - arr[i - 1]) * BigInt(i) * (n - BigInt(i));
+  }
+
+  console.log((result * BigInt(2)).toString());
+
+  //  아래 방법은 O(n^2/2) 이기에 시간초과가 난다
+  //   let result = BigInt(0);
+  //   arr.map((value, i) => {
+  //     for (let index = i + 1; index < n; index++) {
+  //       result += abs(value - arr[index]);
+  //     }
+  //   });
+
+  //   console.log((result * BigInt(2)).toString());
+};
+
+solution(input);
