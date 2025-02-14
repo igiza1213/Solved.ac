586ba0363a3fbf0b58da3b60e47468ae991c3faf
msg2324 <73478662+igiza1213@users.noreply.github.com>
2025-02-14 20:07:10 +0900

    [Silver IV] Title: 점화식, Time: 92 ms, Memory: 9388 KB -BaekjoonHub

---
+++ 백준/Silver/13699. 점화식/점화식.ts
@@ -0,0 +1,28 @@
+import fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "/input.txt";
+var input = fs.readFileSync(__dirname + filePath).toString();
+
+let mem: bigint[] = [BigInt(1)];
+
+function dp(n: number) {
+  let ret = BigInt(0);
+
+  if (mem[n]) ret = mem[n];
+  else {
+    for (let i = 0; i < n; i++) {
+      ret += dp(i) * dp(n - i - 1);
+    }
+
+    mem[n] = ret;
+  }
+
+  return ret;
+}
+
+export const solution = (stdinInput: string) => {
+  const n = +stdinInput.trim();
+
+  console.log(dp(n).toString());
+};
+
+solution(input);
